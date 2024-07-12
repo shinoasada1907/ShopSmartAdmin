d43ac075d7b7746278d482c41e9d74d3b0c2b754
Shinoshinoasada1907 <bonzed2001@gmail.com>
2024-07-12 16:16:27 +0700

    Create Dashboard Admin

---
+++ lib/models/dashboard_btn_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shopsmart_admin/screens/inner_screen.dart/orders/orders_screen.dart';
+import 'package:shopsmart_admin/screens/search_screen.dart';
+import 'package:shopsmart_admin/services/assets_manager.dart';
+
+class DashboardButtonsModel {
+  final String text, imagePath;
+  final Function onPressed;
+
+  DashboardButtonsModel({
+    required this.text,
+    required this.imagePath,
+    required this.onPressed,
+  });
+
+  static List<DashboardButtonsModel> dashboardBtnList(context) => [
+    DashboardButtonsModel(
+          text: "Add a new product",
+          imagePath: AssetsManager.cloud,
+          onPressed: () {},
+        ),
+        DashboardButtonsModel(
+          text: "inspect all products",
+          imagePath: AssetsManager.shoppingCart,
+          onPressed: () {
+            Navigator.pushNamed(context, SearchScreen.routeName);
+          },
+        ),
+        DashboardButtonsModel(
+          text: "View Orders",
+          imagePath: AssetsManager.order,
+          onPressed: () {
+            Navigator.pushNamed(context, OrdersScreenFree.routeName);
+          },
+        ),
+  ];
+}
